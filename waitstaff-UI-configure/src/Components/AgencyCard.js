5e427404318c2289e4fd5854c75165b12bcc609d
teenoh <ugbalaachinike@gmail.com>
2019-07-12 13:32:21 +0100

    create custom agency card

---
+++ waitstaff-UI-configure/src/Components/AgencyCard.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { withStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import SettingsIcon from "@material-ui/icons/Settings";
+import Typography from "@material-ui/core/Typography";
+
+const styles = theme => ({
+  cardContent: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  companyLogo: {
+    height: 100,
+    width: 100,
+    borderRadius: "50%"
+  },
+  newBtn: {
+    background: "linear-gradient(to right, #000051 0%,  #69e2c7 90%)",
+    marginLeft: "auto"
+  },
+  mb4: {
+    marginBottom: theme.spacing(4)
+  },
+  boxShadow: {
+    boxShadow: "1px 1px 3px rgba(201, 201, 201, 0.5)"
+  }
+});
+
+const AgencyCard = ({ classes, setActiveCompany, company }) => {
+    const {id, image, name} = company
+    return (
+      <Card className={classes.card}>
+        <CardHeader
+          action={
+            <IconButton
+              onClick={() => setActiveCompany(company)}
+              aria-label="Settings"
+            >
+              <SettingsIcon />
+            </IconButton>
+          }
+        />
+        <CardContent className={classes.cardContent}>
+          <Avatar src={image} className={classes.companyLogo} />
+          <Typography color="primary" variant="h6">
+            {name}
+          </Typography>
+        </CardContent>
+        <Divider light />
+        <CardActions>
+          <Typography variant="body1">Marvel Comics</Typography>
+          <Button
+            className={classes.newBtn}
+            size="small"
+            color="primary"
+            variant="contained"
+          >
+            New
+          </Button>
+        </CardActions>
+      </Card>
+    );
+}
+
+export default withStyles(styles)(AgencyCard)
